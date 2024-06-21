5dfb1ae73b6a842d71f30b4968fd956f92b75537
Lord-of-Chemistry <wittysage143@gmail.com>
2024-06-21 20:59:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,6 +8,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    return Math.floor( Math.random()*13 ) + 1 // 1-13
+}
 
 function startGame() {
     renderGame()
@@ -34,10 +38,8 @@ function renderGame() {
 
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
-    // Push the card to the cards array
     cards.push(card)
-    console.log(cards)
     renderGame()
 }
